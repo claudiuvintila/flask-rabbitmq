07a2fe9e0a248556cf44b4b526719526a8ac8e0c
claudiuvintila <claudiu.vintila@gmail.com>
2019-03-26 17:50:38 +0200

    added auto_delete argument tu queue declaration

--- flask_rabbitmq/queue.py
+++ flask_rabbitmq/queue.py
@@ -6,7 +6,7 @@ class Queue():
     def __init__(self):
         self._rpc_class_list = []
 
-    def __call__(self, queue=None, type = ExchangeType.DEFAULT, exchange = '', routing_key = '', durable = False):
+    def __call__(self, queue=None, type = ExchangeType.DEFAULT, exchange = '', routing_key = '', durable = False, auto_delete = True):
         """
         当Queue对象被调用时，如@queue()执行的操作
         :param queue_name: 队列名
@@ -14,8 +14,9 @@ class Queue():
         :param exchange_name:
         :param routing_key:
         :param durable:
+        :param auto_delete:
         :return:
         """
         def _(func):
-            self._rpc_class_list.append((type, queue, exchange, routing_key, durable, func))
+            self._rpc_class_list.append((type, queue, exchange, routing_key, durable, auto_delete, func))
         return _
