ec65e5ca7b65ee83e174686c384a40274afa49fa
claudiuvintila <claudiu.vintila@gmail.com>
2020-06-26 14:13:55 +0300

    generated pypi package

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='flask-rabbitmq',
+    name='flask-rabbitmq-cv',
     version='0.0.3',
     author='Pushy',
-    author_email='1437876073@qq.com',
-    url='https://github.com/PushyZqin/flask-rabbitmq',
+    author_email='claudiu.vintila@gmail.com',
+    url='https://github.com/claudiuvintila/flask-rabbitmq',
     description=u'Let rabbitmq use flask development more easy! ! !',
     packages=find_packages(),
     install_requires=['pika']
